478c04091875ac9aad82b54c455f30e7e0ccacc2
LuisDiNicco <luisdini7800@gmail.com>
2026-02-28 18:23:33 -0300

    chore(repo): remove tracked build artifacts and agent files

--- backend/dist/domain/enums/food-source.enum.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.FoodSource = void 0;
-var FoodSource;
-(function (FoodSource) {
-    FoodSource["OPEN_FOOD_FACTS"] = "OPEN_FOOD_FACTS";
-    FoodSource["USER_CUSTOM"] = "USER_CUSTOM";
-})(FoodSource || (exports.FoodSource = FoodSource = {}));
-//# sourceMappingURL=food-source.enum.js.map
